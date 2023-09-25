2f90fec8499a746904013144283c0f58e40b8ff7
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2023-09-25 11:26:31 -0400

    added pagy

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -4,7 +4,7 @@ class BlogPost < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
 
-    scope :sorted, -> { order(Arel.sql("published_at DESC NULLS LAST")).order(updated_at: :desc) }
+    scope :sorted, -> { order(Arel.sql("published_at DESC NULLS FIRST")).order(updated_at: :desc) }
     scope :draft, -> { where(published_at: nil)}
     scope :published, -> { where("published_at <= ?", Time.current)}
     scope :scheduled, -> { where("published_at > ?", Time.current)}
